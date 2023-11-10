087d4464a549e1511bda1e2533d7d63aa32c0be0
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-11-10 07:06:12 +0530

    Minor Change

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: '<your user>', // usually postgres
+    user: '<your user>', // default is postgres
     password: '<your postgres password>',
     host: 'localhost',
     port: 5432, // default port of postgres is 5432
